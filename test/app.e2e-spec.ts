484bb34574509fae37be7935ef257b38b640d155
Ujwal Dhakal <kevin.ujwal@gmail.com>
2021-06-06 14:52:45 +0545

    Added Event versioning

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -75,7 +75,7 @@ describe('AppController (e2e)', () => {
     });
   });
 
-  it.only('/ (PUT) should transfer the item and reduce quantity at hands', async () => {
+  it('/ (PUT) should transfer the item and reduce quantity at hands', async () => {
     const id = uuidv4();
     const payload = {
       id: id,
